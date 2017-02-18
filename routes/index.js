d8b178c0beb42268414deb495603a0745ee8cac5
Ahmed Zaman <ahmedrzaman@gmail.com>
2017-02-18 12:37:32 +0000

    fs.readFileSync attempt in local directory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function (req, res, next) {
+  try {
+    var data = fs.readFileSync(path.join(__dirname, req.body.path || 'users.js'), 'utf-8');
+    res.send(data);
+  } catch (ex) {
+    res.send(ex);
+  }
 });
 
 module.exports = router;
